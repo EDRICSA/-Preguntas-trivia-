0b2b2b5aef3a44425bcd3f4007d3f55d68c5e6dc
EDRICSA <105190587+EDRICSA@users.noreply.github.com>
2022-06-07 20:54:47 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,61 @@
+export const data = [
+    {
+      question: "¿Cuál es el lugar más frío de la tierra?",
+      choices: ["Canadá ", "Islandia", "Rusia", "Antártida"],
+      answer: "Antártida",
+    },
+  
+    {
+      question: "¿Quién escribió La Odisea?",
+      choices: ["Miguel de cervantes", "Homero", "Mario Vargas Llosa", "William shakespere"],
+      answer: "Homero",
+    },
+  
+    {
+      question: "¿De qué Pais son originarios los Beatles?",
+      choices: ["Rusia", "Alemania", "Estados unidos", "Inglaterra", ],
+      answer: "Inglaterra",
+    },
+  
+    {
+      question: "¿Cuál es el pais mas grande del mundo?",
+      choices: ["Rusia", "Alaska", "Estados unidos", "Brasil"],
+      answer: "Rusia",
+    },
+  
+    {
+      question: "¿Cuál es la flor nacional de Japón?",
+      choices: ["Flor de Cerezo", "Flor de loto", "Gardemia", "Girasol"],
+      answer: "Flor de Cerezo",
+    },
+  
+    {
+      question: "¿Cuál es el disco más vendido de la historia?",
+      choices: ["Equals de Ed Sheeran ", "The dark side of the moon de Pink Floyd", "Thriller de Michael Jackson", "Back in black de AC/DC "],
+      answer: "Thriller de Michael Jackson",
+    },
+  
+    {
+      question: "¿Cuál es la serie de libros mejor vendida del siglo 21?",
+      choices: ["Harry Potter", "Star wars", "El señor de los aillos"],
+      answer: "Harry Potter",
+    },
+  
+    {
+      question: "¿Cuál es el país más pequeño del mundo? ",
+      choices: ["Mónaco", "San Marino", "San Cristóbal y Nieves", "El Vaticano", ],
+      answer: "El Vaticano",
+    },
+  
+    {
+      question: "¿Cuántos días le toma a la tierra dar una vuelta a la órbita del sol?",
+      choices: ["459", "1000", "365", "245", ],
+      answer: "365 ",
+    },
+  
+    {
+      question: "¿Cual es la cancion mas escuchada en spotify?",
+      choices: ["Running up that hill de Kate bush", "Shape of You de Ed sheeran", "Thriller de Michael Jackson", "San lucas de Kevin karl"],
+      answer: "Shape of You de Ed sheeran",
+    },
+  ];
